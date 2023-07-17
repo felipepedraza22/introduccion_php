3353f4b3308da50dc7e69bfd485fd4f3c2ec041b
felipepedraza22 <carlospedraza@colegiooguanenta.edu.co>
2023-07-17 18:22:35 -0500

    Se agrego un archivo

---
+++ 3_operadores.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Operadores</h1>
+    <?php
+    //OPeradores aritmetícos 
+
+    //OPeradores de comparación
+
+    //Operadores lógiocos
+
+    //Operadores de unión de cadenas 
+    ?>
+</body>
+</html>
